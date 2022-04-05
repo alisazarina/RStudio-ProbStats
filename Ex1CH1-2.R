fff669f6236fe591934c8b5c44d2666b518dff0f
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-05 22:55:00 +0800

    Update Ex1CH1-2.R

--- Ex1CH1-2.R
+++ Ex1CH1-2.R
@@ -7,12 +7,14 @@ score=c(464, 463, 448, 545, 422, 502,
 
 mean(score)
 median(score)
+sort(score)                   # check if median is correct or not -- extra note
 range(score)
 var(score)
 sd(score)
+summary(score)                # gives a summary of the data (Min, Q1, Median, Mean, Q3, Max) -- extra note
 
-library(modeest)
-mfv(score)   #mode
+library(modeest)              # need to do this to activate the mode function
+mfv(score)                    # mode
 
 
 #Q2
@@ -21,16 +23,17 @@ days=c(11, 14, 18, 7, 11, 16, 31, 6, 11, 8, 9)
 mean(days)
 median(days)
 range(days)
-r = 31-6        #RANGE
-r               #PRINT RANGE
+r = 31-6                      # RANGE (manual)
+r                             # PRINT RANGE
 var(days)
 sd(days)
 
 library(modeest)
-mfv(days)  #mode
+mfv(days)                     # mode
 
 #To find interquartile range.
 summary(days)
-IQR(days.na.rm=FALSE) #Interquartile
-IQR_1 = 15 - 8.5
-IQR_1
+IQR(days,na.rm=FALSE)         # Interquartile (auto) = 6.5
+IQR_1 = 15 - 8.5              # Interquartile (manual)
+IQR_1                         # = 6.5
+  
