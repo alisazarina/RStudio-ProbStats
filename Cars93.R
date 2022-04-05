1dcabcbba7950fa190bcdb7b4344c84ba6f963bf
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-05 22:56:26 +0800

    Update Cars93.R

--- Cars93.R
+++ Cars93.R
@@ -1,12 +1,14 @@
-library(MASS)
-data("Cars93")
+# NOTE: You need to run EACH LINE of code (CTRL + ENTER)
+
+library(MASS)                                                                         # call the library "MASS"
+data("Cars93")                                                                        # use the data "Cars93"
 
 png(file = "graph.jpg")
 
-# Construct a table
+#construct a table
 table(Cars93$Type)
 
-# Draw Barplot
+#draw barplot
 barplot(table(Cars93$Type))
 barplot(table(Cars93$Type), main = "Frequency vs Types of Cars93",
         ylab = "Frequency", xlab = "Types of Cars",
@@ -14,37 +16,37 @@ barplot(table(Cars93$Type), main = "Frequency vs Types of Cars93",
         col = c("red", "blue", "yellow", "black", "green", "purple"),
         space = 0.05)
 
-# Draw Piechart
+#draw piechart
 pie(table(Cars93$Type))
 pie(table(Cars93$Type), main = "Types of Cars93")
 
-lbls = paste(names(table(Cars93$Type)), "\n", table(Cars93$Type), sep = "")
+lbls = paste(names(table(Cars93$Type)), "\n", table(Cars93$Type), sep = "")           # sep = separate the values
 pie(table(Cars93$Type), labels = lbls, main = "Types of Cars93 \n (with frequency)",
     border = "red", lty = "dotted")
 
 lbls2 = paste(table(Cars93$Type))
-pie(table(Cars93$Type), labels = lbls, main = "Types of Cars93 \n (with frequency)",
+pie(table(Cars93$Type), labels = lbls2, main = "Types of Cars93 \n (with frequency)",
     border = "red", lty = "dotted", col = rainbow(length(table(Cars93$Type))))
 
-lgd = names(table(Cars93$Type))
-legend("topright", lgd, cex = 0.5, fill = rainbow(length(table(Cars93$Type))) )
+lgd = names(table(Cars93$Type))                                                       # insert legend
+legend("topright", lgd, cex = 0.5, fill = rainbow(length(table(Cars93$Type))) )       # decorate the legend
 
-library(plotrix)
+library(plotrix)                                                                      # change to 3D
 pie3D(table(Cars93$Type), labels = lbls, main = "Types of Cars93 \n (with frequency)",
     border = "red", lty = "dotted")
 
-# Draw Histogram
+#Histogram
 hist(Cars93$Price)
 hist(Cars93$Price, xlab = "Price (x $1,000)", xlim = c(0,70),
      main = "Prices of 93 Models of 1993 Cars", probability = TRUE)
-lines(density(Cars93$Price))
+lines(density(Cars93$Price))                                                          # insert lines into histogram
 
-# Draw Scatter plot
-plot(Cars93$Horsepower~Cars93$MPG.city)
+#Scatter plot
+plot(Cars93$Horsepower~Cars93$MPG.city)                                               # need 2 data for scatter plot (use "~" to connect)
 plot(Cars93$Horsepower~Cars93$MPG.city, xlab = "Horsepower", ylab = "MPG City",
      main = "MPG City vs Horsepower")
 
-# Draw Boxplot
+#Boxplot
 boxplot(Cars93$Horsepower~Cars93$Cylinders)
 boxplot(Cars93$Horsepower~Cars93$Cylinders, xlab = "Cylinders", ylab = "Horsepower",
         main = "Horsepower vs Number of cylinders")
